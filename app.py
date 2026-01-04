0cd068f077bea96f89efb1cdb459b8af504a4b30
heldielclassico <113118436+heldielclassico@users.noreply.github.com>
2026-01-04 17:32:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Route untuk halaman utama
 @app.route('/')
-def index():
+def home():
+    # Mengarahkan ke file index.html di dalam folder templates
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Debug mode diaktifkan agar server otomatis restart saat ada perubahan kode
+    app.run(debug=True, port=5000)
